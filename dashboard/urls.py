7b431dfb96ae9908bf79c18af8f3d3f6af380231
nitinpaul <nitinpaul2811@gmail.com>
2018-10-05 14:58:32 +0530

    Added urlpatters and view templates for dashboard app

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,5 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard_login, name='dashboard_login'),
+    # /dashboard/
+    path('', views.dashboard_home, name='dashboard_home'),
+
+    # /dashboard/login/
+    path('login/', views.dashboard_login, name='dashboard_login'),
+
+    # ex: /dashboard/volunteer/
+    path('<role>/', views.dashboard_role, name='dashboard_role'),
+
+    # ex: /dashboard/volunteer/submit-questions/
+    path('<role>/<action>/', views.dashboard_action, name='dashboard_action'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -2,5 +2,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def dashboard_home(request):
+    return HttpResponse("Dashboard Home")
+
 def dashboard_login(request):
-    return HttpResponse("Login to access the dashboard!")
+    return HttpResponse("Dashboard Login")
+
+def dashboard_role(request, role):
+    return HttpResponse(role + " Dashboard")
+
+def dashboard_action(request, role, action):
+    return HttpResponse(role + " performed " + action)
