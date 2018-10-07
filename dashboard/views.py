bcd123a9a5164f61bd84f4bc1b097503ee0b79c1
nitinpaul <nitinpaul2811@gmail.com>
2018-10-07 20:59:08 +0530

    Refactored the view function to use the shortcut syntax to render the view

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def dashboard_home(request):
-    return HttpResponse("Dashboard Home")
+    context = {}
+    return render(request, 'dashboard/home.html', context)
 
 def dashboard_login(request):
     return HttpResponse("Dashboard Login")
