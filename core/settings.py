4fab9694891c40848d8e7a7bd66314a12ae4f870
nitinpaul <nitinpaul2811@gmail.com>
2018-10-08 15:23:56 +0530

    Added bootstrap static files and related config

--- core/settings.py
+++ core/settings.py
@@ -112,3 +112,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'bootstrap'),
+    os.path.join(BASE_DIR, 'assets'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
