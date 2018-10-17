50e7d396f5d6a01a2e13d80f7365b1c2255baedc
nitinpaul <nitinpaul2811@gmail.com>
2018-10-17 13:47:02 +0530

    Set DEBUG to False for testing

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('sawaliram_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
