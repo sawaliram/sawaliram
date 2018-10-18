b85926b8b1cb1120bd128612cca248b81203c227
nitinpaul <nitinpaul2811@gmail.com>
2018-10-18 12:41:49 +0530

    Added settings for static files management

--- core/settings.py
+++ core/settings.py
@@ -120,7 +120,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
