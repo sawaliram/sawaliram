d7c04b08a5bb292acda513ac5426f96ceb242d3a
nitinpaul <nitinpaul2811@gmail.com>
2018-10-26 14:24:58 +0530

    Added redirect from root url to dashboard home

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='dashboard/', permanent=False)),
     path('dashboard/', include('dashboard.urls')),
     path('admin/', admin.site.urls),
 ]
