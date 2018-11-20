8e9bee8bf0a3d8b3f72f096dc34f159038187577
nitinpaul <nitinpaul2811@gmail.com>
2018-11-20 14:16:16 +0530

    Added pylint formatting

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,36 +1,37 @@
-
-from django.db import models
+"""Define the data models for questions, answers and other components of the data."""
 import datetime
+from django.db import models
 
 class Question(models.Model):
+	"""Define the data model for a question."""
 
-    class Meta:
-        db_table = 'question'
+	class Meta:
+		db_table = 'question'
 
-    school = models.CharField(max_length=100)
-    area = models.CharField(max_length=100)
-    state = models.CharField(max_length=100, default='')
-    student_name = models.CharField(max_length=100)
-    student_class = models.IntegerField(default=0)
-    student_age = models.IntegerField(default=0)
-    question_text = models.CharField(max_length=200)
-    question_text_english = models.CharField(max_length=200, default='')
-    question_topic = models.CharField(max_length=100)
-    question_format = models.CharField(max_length=100)
-    question_language = models.CharField(max_length=100)
-    contributor = models.CharField(max_length=100)
-    contributor_role = models.CharField(max_length=100)
-    context = models.CharField(max_length=100)
-    local_language = models.CharField(max_length=100)
-    medium_language = models.CharField(max_length=100)
-    curriculum_followed = models.CharField(max_length=100, default='')
-    published = models.BooleanField(default=False)
-    published_source = models.CharField(max_length=200, default='')
-    published_date = models.DateField(default=datetime.date.today)
-    question_asked_on = models.DateField(null=True)
-    notes = models.CharField(max_length=500, default='')
-    created_on = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(auto_now=True)
+	school = models.CharField(max_length=100)
+	area = models.CharField(max_length=100)
+	state = models.CharField(max_length=100, default='')
+	student_name = models.CharField(max_length=100)
+	student_class = models.IntegerField(default=0)
+	student_age = models.IntegerField(default=0)
+	question_text = models.CharField(max_length=200)
+	question_text_english = models.CharField(max_length=200, default='')
+	question_topic = models.CharField(max_length=100)
+	question_format = models.CharField(max_length=100)
+	question_language = models.CharField(max_length=100)
+	contributor = models.CharField(max_length=100)
+	contributor_role = models.CharField(max_length=100)
+	context = models.CharField(max_length=100)
+	local_language = models.CharField(max_length=100)
+	medium_language = models.CharField(max_length=100)
+	curriculum_followed = models.CharField(max_length=100, default='')
+	published = models.BooleanField(default=False)
+	published_source = models.CharField(max_length=200, default='')
+	published_date = models.DateField(default=datetime.date.today)
+	question_asked_on = models.DateField(null=True)
+	notes = models.CharField(max_length=500, default='')
+	created_on = models.DateTimeField(auto_now_add=True)
+	updated_on = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.question_text
+	def __str__(self):
+		return self.question_text
