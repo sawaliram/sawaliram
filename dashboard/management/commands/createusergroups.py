6df4d6c1522c991f2b5c6860e4038379d8c76ce4
nitinpaul <nitinpaul2811@gmail.com>
2019-04-01 12:59:41 +0530

    Removed unnecesary code

--- dashboard/management/commands/createusergroups.py
+++ dashboard/management/commands/createusergroups.py
@@ -2,16 +2,13 @@
 Create user groups and set permissions
 """
 from django.core.management import BaseCommand
-from django.contrib.auth.models import Group, Permission
-from django.contrib.contenttypes.models import ContentType
-from dashboard.models import User
+from django.contrib.auth.models import Group
+
 
 class Command(BaseCommand):
-    help="Creates user groups and sets group permissions"
+    help = "Creates user groups and sets group permissions"
 
     def handle(self, *args, **options):
-
-        # groups
         volunteers = Group.objects.get_or_create(name='volunteers')
         editors = Group.objects.get_or_create(name='editors')
         experts = Group.objects.get_or_create(name='experts')
