4cab961955be12713101c593765ddf9a0b1f3edc
Hippo <badrihippo@gmail.com>
2019-05-19 18:36:25 +0530

    Implemented Django "messages" infrastructure
    Now, you can "flash" notifications to users by using the commands
    under django.contrib.messages

--- dashboard/views.py
+++ dashboard/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import Group
+from django.contrib import messages
 from django.db.models import Max, Subquery
 import pandas as pd
 from dashboard.models import (
