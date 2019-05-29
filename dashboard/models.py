36d11d5cca6183a89679ff825bce8da580c1b3d8
Raul9595 <rahulsvarm@gmail.com>
2019-05-29 18:46:35 +0530

    Updated the reduce the max_length of the language field
    Updated the reduce the max_length of the language field

--- dashboard/models.py
+++ dashboard/models.py
@@ -232,6 +232,6 @@ class TranslatedQuestion(models.Model):
         on_delete=models.PROTECT,
         default='')
     question_text = models.CharField(max_length=1000)
-    language = models.CharField(max_length=1000)
+    language = models.CharField(max_length=100)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
