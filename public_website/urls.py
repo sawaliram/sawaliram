525128b82370d05b3678d5bd998f91ebb760ce2f
nitinpaul <nitinpaul2811@gmail.com>
2019-09-24 16:35:17 +0530

    Add Home Page

--- public_website/urls.py
+++ public_website/urls.py
@@ -2,12 +2,10 @@
 
 from django.urls import path
 from . import views
-from django.views.generic import RedirectView
 
 app_name = 'public_website'
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='dashboard/', permanent=False), name='home'),
     path('user/<int:user_id>', views.UserProfileView.as_view(), name='user-profile'),
-    # path('', views.HomeView.as_view(), name='home')
+    path('', views.HomeView.as_view(), name='home')
 ]
