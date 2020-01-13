f00b9f99082a3d874eaa29566d98e0086c3be739
Badri Sunderarajan <badrihippo@gmail.com>
2020-01-13 17:14:44 +0530

    Pick one translation draft if multiple are available

--- dashboard/views.py
+++ dashboard/views.py
@@ -1072,13 +1072,23 @@ class BaseEditTranslation(UpdateView):
         source = get_object_or_404(self.source_model,
             id=self.kwargs.get('source'))
 
-        obj, createdp = self.model.objects.get_or_create(
-            article=source,
-            translated_by=self.request.user,
-            language=self.kwargs.get('lang_to'),
-        )
+        # Fetch or create the translation object itself
 
-        obj.article.set_language(self.kwargs.get('lang_from'))
+        query_filters = {
+            'article': source,
+            'translated_by': self.request.user,
+            'language': lang_to,
+        }
+
+        try:
+            obj, createdp = (self.model.objects
+                .get_or_create(**query_filters))
+        except self.model.MultipleObjectsReturned:
+            # Workaround: select the first object
+            # TODO: handle this more systematically
+            obj = self.model.objects.filter(**query_filters)[0]
+
+        obj.article.set_language(lang_from)
 
         return obj
 
@@ -1162,12 +1172,6 @@ class EditArticleTranslation(BaseEditTranslation):
         if obj.body == None: obj.body = ''
 
         return obj
-
-    def get_conflict_url(self):
-        # TODO: redirect to conflict resolution page instead
-        return reverse('public_website:view-article', kwargs={
-            'article': self.kwargs.get('source')
-        })
 
 # Legacy Functions
 
