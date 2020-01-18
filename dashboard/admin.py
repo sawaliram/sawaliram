f24e3dd21d6b6e48d1325279a0bcafbda35ba538
Badri Sunderarajan <badrihippo@gmail.com>
2020-01-18 16:46:33 +0530

    Display more columns in admin site

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -6,9 +6,21 @@ from .models import (
 
 @admin.register(Answer)
 class AnswerAdmin(admin.ModelAdmin):
+    def question_text_english(obj):
+        return (obj.question_id.question_text_english
+            or obj.question_id.question_text)
+    question_text_english.short_description = 'Question (en)'
+    list_display = [
+        'question_id',
+        question_text_english,
+        'status',
+        'submitted_by',
+        'created_on',
+    ]
     search_fields = ['question_id__id', 'question_id__question_text']
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
-    search_fields = ['id', 'question_text']
+    search_fields = ['id', 'question_text', 'question_text_english']
     list_filter = ['question_language', 'state', 'field_of_interest']
+    list_display = ['id', 'question_text', 'question_text_english', 'field_of_interest', 'area', 'state']
