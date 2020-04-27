51863c494ec192ce1def7d06c628e5472ab2ea85
Badri Sunderarajan <badrihippo@gmail.com>
2020-04-27 18:18:12 +0530

    Add published_on date to Answers

--- dashboard/models.py
+++ dashboard/models.py
@@ -238,6 +238,7 @@ class Answer(models.Model):
         null=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
+    published_on = models.DateTimeField(blank=True, null=True)
 
     comments = GenericRelation('dashboard.Comment')
 
