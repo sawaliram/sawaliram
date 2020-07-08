4bd8822aebe005ae73f2c1f6acce8ea7ad3ca0e5
nitinpaul <nitinpaul2811@gmail.com>
2020-07-08 14:42:18 +0530

    Add review count and meta serach feature to Review Answers

--- dashboard/views.py
+++ dashboard/views.py
@@ -636,7 +636,8 @@ class ReviewAnswersList(SearchView):
                     Q(school__icontains=request.GET.get('q')) |
                     Q(area__icontains=request.GET.get('q')) |
                     Q(state__icontains=request.GET.get('q')) |
-                    Q(field_of_interest__icontains=request.GET.get('q'))
+                    Q(field_of_interest__icontains=request.GET.get('q')) |
+                    Q(published_source__icontains=request.GET.get('q'))
             )
             return results
         else:
