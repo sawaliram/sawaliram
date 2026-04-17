f989bf95de97410487ed5de7e348841163374279
subindevs <36504048+subindevs@users.noreply.github.com>
2026-04-17 14:40:43 +0100

    Fix migration syntax error caused by locally-scoped draftable mixins
    Django's migration serializer cannot reference classes defined inside
    function closures (<locals>). Moved DraftMixin, SubmittedMixin, and
    PublishedMixin to module level in draftables.py, replacing closure-scoped
    `cls` references with `type(self)`. Regenerated migration 0038 which now
    has valid, importable bases references.

--- dashboard/mixins/draftables.py
+++ dashboard/mixins/draftables.py
@@ -23,6 +23,49 @@ class SubmittedDraftableManager(models.Manager):
             .get_queryset()
             .filter(status=self.model.STATUS_SUBMITTED))
 
+
+class DraftMixin:
+
+    def __init__(self, *args, **kwargs):
+        self._meta.get_field('status').default = self.STATUS_DRAFT
+        super().__init__(*args, **kwargs)
+
+    def submit_draft(self):
+        try:
+            self.status = self.STATUS_SUBMITTED
+            self.save()
+        except Exception as e:
+            print('Error submitting: {}'.format(e))
+            raise e
+        return type(self).objects.get(id=self.id)
+
+
+class SubmittedMixin:
+
+    def __init__(self, *args, **kwargs):
+        self._meta.get_field('status').default = self.STATUS_SUBMITTED
+        super().__init__(*args, **kwargs)
+
+    def publish(self, approved_by):
+        try:
+            published = type(self).objects.get(id=self.id)
+            published.approved_by = approved_by
+            published.status = self.STATUS_PUBLISHED
+            published.published_on = datetime.now()
+            published.save()
+        except Exception as e:
+            print('Error publishing: {}'.format(e))
+            raise e
+        return published
+
+
+class PublishedMixin:
+
+    def __init__(self, *args, **kwargs):
+        self._meta.get_field('status').default = self.STATUS_PUBLISHED
+        super().__init__(*args, **kwargs)
+
+
 class DraftableModel(models.Model):
     '''
     Generic mixin for all "draftable" items - that is, items which go
@@ -66,69 +109,12 @@ class DraftableModel(models.Model):
 
     @classmethod
     def get_draft_model(cls):
-        class DraftMixin:
-
-            def __init__(self, *args, **kwargs):
-                self._meta.get_field('status').default = self.STATUS_DRAFT
-                super().__init__(*args, **kwargs)
-
-            def submit_draft(self):
-                '''
-                Submit the draft. That is, change the status of the
-                current object to STATUS_SUBMITTED and return it.
-                '''
-
-                try:
-                    self.status = self.STATUS_SUBMITTED
-                    self.save()
-                except Exception as e:
-                    error = 'Error submitting: {}'.format(e)
-                    print(error)
-                    raise e
-
-                return cls.objects.get(id=self.id)
-
-        # Return the mixin
         return DraftMixin
 
     @classmethod
     def get_submitted_model(cls):
-        class SubmittedMixin:
-
-            def __init__(self, *args, **kwargs):
-                self._meta.get_field('status').default = self.STATUS_SUBMITTED
-                super().__init__(*args, **kwargs)
-
-            def publish(self, approved_by):
-                '''
-                Publish the submission. That is, change the status
-                of the current object to STATUS_PUBLISHED and set
-                the approved_by attribute
-                '''
-
-                try:
-                    published = cls.objects.get(id=self.id)
-                    published.approved_by = approved_by
-                    published.status = self.STATUS_PUBLISHED
-                    published.published_on = datetime.now()
-                    published.save()
-                except Exception as e:
-                    error = 'Error publishing: {}'.format(e)
-                    print(error)
-                    raise e
-
-                return published
-
-        # Return the mixin
         return SubmittedMixin
 
     @classmethod
     def get_published_model(cls):
-        class PublishedMixin:
-
-            def __init__(self, *args, **kwargs):
-                self._meta.get_field('status').default = self.STATUS_PUBLISHED
-                super().__init__(*args, **kwargs)
-
-        # Return the mixin
         return PublishedMixin
